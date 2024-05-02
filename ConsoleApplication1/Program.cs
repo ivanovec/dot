c189a48acf9353caca9ffdfd37cb49b0cf46ba19
Ivan Efimov <ivan.efimov@jetbrains.com>
2024-05-02 12:41:24 +0300

    123

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     internal class Program
     {
         private int c;
         public static void Main(string[] args)
         {
+            var a = 1;
+            Console.WriteLine(a);
             while (true) ;
         }
     }
